9bc180d79962d7b1ea74afea6100ce86b7c18c88
Hanninenjp <jphanninen@gmail.com>
2017-03-30 11:53:07 +0300

    Blocker game development version, added game character flapping animation

--- bg-app.js
+++ bg-app.js
@@ -10,24 +10,24 @@
     var gameScore;
 
     function startGame() {
-        myGameArea.start();
-        gameCharacter = new characterImageComponent(34, 30, "./images/c-frame-1.png", 5, 120);
-        gameBackground = new backgroundImageComponent(540, 270, "./images/beach-background.png");
-        gameScore = new component("30px", "Consolas", "black", 280, 40, "text");
+        gameArea.start();
+        gameCharacter = new CharacterImageComponent(34, 30, "./images/c-frame-?.png", 4, 5, 120);
+        gameBackground = new BackgroundImageComponent(540, 270, "./images/beach-background.png");
+        gameScore = new Component("30px", "Consolas", "black", 280, 40, "text");
     }
 
-    var myGameArea = {
+    var gameArea = {
         start : function() {
             this.canvas = document.getElementById("bg-canvas");
             this.context = this.canvas.getContext("2d");
             this.frameCount = 0;
             this.interval = setInterval(updateGameArea, 20);
             window.addEventListener('keydown', function(e){
-                myGameArea.keys = (myGameArea.keys || []);
-                myGameArea.keys[e.keyCode] = (e.type === "keydown");
+                gameArea.keys = (gameArea.keys || []);
+                gameArea.keys[e.keyCode] = (e.type === "keydown");
             });
             window.addEventListener('keyup', function(e){
-                myGameArea.keys[e.keyCode] = (e.type === "keydown");
+                gameArea.keys[e.keyCode] = (e.type === "keydown");
             });
         },
         clear : function() {
@@ -38,14 +38,7 @@
         }
     };
 
-    function everyInterval(n){
-        if((myGameArea.frameCount / n) % 1 === 0){
-            return true;
-        }
-        return false;
-    }
-
-    function component(width, height, color, x, y, type){
+    function Component(width, height, color, x, y, type){
         this.type = type;
         this.width = width;
         this.height = height;
@@ -54,7 +47,7 @@
         this.x = x;
         this.y = y;
         this.update = function() {
-            var ctx = myGameArea.context;
+            var ctx = gameArea.context;
             if (this.type === "text"){
                 ctx.font = this.width + " " + this.height;
                 ctx.fillStyle = color;
@@ -71,7 +64,7 @@
         };
     }
 
-    function backgroundImageComponent(width, height, source){
+    function BackgroundImageComponent(width, height, source){
         this.width = width;
         this.height = height;
         this.speedX = 0;
@@ -81,7 +74,7 @@
         this.image = new Image();
         this.image.src = source;
         this.update = function(){
-            var ctx = myGameArea.context;
+            var ctx = gameArea.context;
             ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
             ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
         };
@@ -94,17 +87,24 @@
         };
     }
 
-    function characterImageComponent(width, height, source, x, y){
+    function CharacterImageComponent(width, height, source, frames, x, y){
         this.width = width;
         this.height = height;
         this.speedX = 0;
         this.speedY = 0;
         this.x = x;
         this.y = y;
+        this.source = source;
+        this.frames = frames;
+        this.frame = 1;
         this.image = new Image();
-        this.image.src = source;
+        this.image.src =  this.source.replace("?", this.frame);
         this.update = function(){
-            var ctx = myGameArea.context;
+            var ctx = gameArea.context;
+            if(gameArea.frameCount !== 0 && (gameArea.frameCount % 10) === 0){
+                this.frame = this.frame < this.frames ? this.frame + 1 : 1;
+            }
+            this.image.src =  this.source.replace("?", this.frame);
             ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
         };
         this.newPos = function(){
@@ -139,39 +139,39 @@
         var x, height, gap, minHeight, maxHeight, minGap, maxGap;
         for (var i = 0; i < gameObstacles.length; i += 1) {
             if (gameCharacter.collideWith(gameObstacles[i])) {
-                myGameArea.stop();
+                gameArea.stop();
                 return;
             }
         }
-        myGameArea.clear();
-        myGameArea.frameCount += 1;
+        gameArea.clear();
+        gameArea.frameCount += 1;
         gameBackground.speedX = -1;
         gameBackground.newPos();
         gameBackground.update();
-        if (myGameArea.frameCount === 1 || everyInterval(150)) {
-            x = myGameArea.canvas.width;
+        if (gameArea.frameCount === 1 || (gameArea.frameCount % 150) === 0) {
+            x = gameArea.canvas.width;
             minHeight = 20;
-            maxHeight = 135;
+            maxHeight = 160;
             height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
             minGap = 50;
             maxGap = 80;
             gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
-            gameObstacles.push(new component(10, height, "green", x, 0));
-            gameObstacles.push(new component(10, x - height - gap, "green", x, height + gap));
+            //Obstacles are currently never removed from the array, even after they leave the canvas
+            gameObstacles.push(new Component(10, height, "green", x, 0));
+            gameObstacles.push(new Component(10, x - height - gap, "green", x, height + gap));
         }
         for (i = 0; i < gameObstacles.length; i += 1) {
             gameObstacles[i].x += -1;
             gameObstacles[i].update();
         }
-        gameScore.text = "SCORE: " + myGameArea.frameCount;
+        gameScore.text = "SCORE: " + gameArea.frameCount;
         gameScore.update();
         gameCharacter.speedY = 0;
         gameCharacter.speedX = 0;
-        if (myGameArea.keys && myGameArea.keys[37]) {gameCharacter.speedX = -1; }
-        if (myGameArea.keys && myGameArea.keys[39]) {gameCharacter.speedX = 1; }
-        if (myGameArea.keys && myGameArea.keys[38]) {gameCharacter.speedY = -1; }
-        if (myGameArea.keys && myGameArea.keys[40]) {gameCharacter.speedY = 1; }
+        if (gameArea.keys && gameArea.keys[37]) {gameCharacter.speedX = -1; }
+        if (gameArea.keys && gameArea.keys[39]) {gameCharacter.speedX = 1; }
+        if (gameArea.keys && gameArea.keys[38]) {gameCharacter.speedY = -1; }
+        if (gameArea.keys && gameArea.keys[40]) {gameCharacter.speedY = 1; }
         gameCharacter.newPos();
         gameCharacter.update();
-
     }
